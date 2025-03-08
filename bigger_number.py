9c7dd14b5384482827586f68b33bc6fe315de3cf
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-09 07:51:04 +0800

    Added program_one.py

---
+++ bigger_number.py
@@ -0,0 +1,9 @@
+#ask user for input
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+
+#in which condition will it apply
+if num1 < num2:
+    print(f"{num2} is greater than {num1}")
+else:
+    print(f"{num1} is greater than {num2}")
