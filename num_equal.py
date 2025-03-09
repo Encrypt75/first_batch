3c98a9896496cdea6f9d730b9176060f828e0478
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-09 21:18:19 +0800

    Add files via upload
    finds out if the 2 inputs are 'equal' or 'not equal'

---
+++ num_equal.py
@@ -0,0 +1,9 @@
+#ask user for input
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+
+#conditional statements: is it equal
+if num1 == num2:
+    print(f"{num2} is equal to {num1}")
+else:
+    print("Not Equal")
