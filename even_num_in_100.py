df1e4bbac58015e11481c0f4a9e85b4dadb0e435
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-12 12:49:54 +0800

    even_numbers_from_zero_to_ahundred
    This program prints only even numbers from 0 to 100 using for-loop

---
+++ even_num_in_100.py
@@ -0,0 +1,6 @@
+#prints all the even nums starting from 0 to 100 (Use for-loop)
+for count_even in range(0, 101):
+    #if count_even is divisible by 2
+    if count_even % 2 == 0:
+        print(count_even)
+print("==End==\n")
