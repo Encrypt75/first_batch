03fdd45ee7e7bea8586f6f1ab46b1705b8fcee81
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-13 11:25:42 +0800

    no_num_with_zero_and_five_2
    This program disregards every numbers from 0 to 100 with "0" and "5"

---
+++ no_num_with_zero_and_five.py
@@ -0,0 +1,11 @@
+#initialization
+cnt = 0
+#while count is less than 100
+while cnt < 100:
+    cnt += 1 #increment
+    if cnt % 2 != 0:
+        pass 
+        if cnt % 5 != 0:
+            print(cnt)
+    else:
+        continue
