c5223befb8a924086817678c9493aa2759754402
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-13 11:12:51 +0800

    smaller_num_2
    This program finds the smallest number between two inputs

---
+++ smaller_num.py
@@ -0,0 +1,9 @@
+#ask user for input
+num1 = float(input("Enter num1: "))
+num2 = float(input("Enter num2: "))
+
+#in which condition will it apply
+if num1 > num2:
+    print(f"{num2} is less than {num1}")
+else:
+    print(f"{num1} is less than {num2}")
