c8aa93e127046c9e873bc1d7be570d842837d025
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-13 11:19:09 +0800

    remainder_of_num_2
    This program finds the remainder of two inputs

---
+++ remainder_of_num.py
@@ -0,0 +1,6 @@
+#ask user for input
+num1 = float(input("Enter num1: "))
+num2 = float(input("Enter num2: "))
+
+#prints only the remainder using modulus division(%)
+print(f"Remainder: {num1%num2}\n")
