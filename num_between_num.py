119594564351cd871a62cbf7da82cadb95801eb1
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-13 11:27:15 +0800

    num_between_num_2
    This program lets the user input 2 numbers, first num which is the starting point and second as the nth term

---
+++ num_between_num.py
@@ -0,0 +1,7 @@
+#ask user to input 2 numbers
+num1 = int(input("num1: "))
+num2 = int(input("num2: "))
+
+#for-loop that starts from num1 to last(num2)
+for prnt in range(num1 + 1, num2):
+    print(prnt)
