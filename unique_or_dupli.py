4f28175cd08e736462364e0525505882ffd3d72c
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-15 09:05:11 +0800

    unique_or_dupli_3
    This program asks for 4-digit numbers and it checks if there are repeated numbers in the in the number (duplicated) or not (unique).

--- unique_or_dupli.py
+++ unique_or_dupli.py
@@ -1,16 +1,28 @@
-#storage for entered number
-entered_number = set()
-
-#check if the value is valid
+#ask user for input until the value is valid
 while True:
     try:
-        #the user must input numbers
-        ask = int(input("Enter number(####): "))
+        ask_input = int(input("Enter number(####): "))
     except ValueError:
         print("Value is invalid")
     else:
-        if ask in entered_number:
-            print("duplicated")
-        else:
+        # Convert the number to a string
+        number_str = str(ask_input)
+
+        # Use a set to track seen digits
+        seen_digits = set()
+        is_unique = True
+
+        # Iterate over each digit
+        for digit in number_str:
+            if digit in seen_digits:
+                is_unique = False
+                break
+            seen_digits.add(digit)
+
+        #print statement
+        if is_unique:
             print("unique")
-            entered_number.add(ask)
+        else:
+            print("duplicated")
+
+        break
