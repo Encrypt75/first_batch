2854fe9796e40d0d3cf8912e8b92c73f1cca6f91
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-15 14:02:47 +0800

    smallest_to_highes_3
    This program shows the arranged values from least to greatest numbers, and asks for another input if input is invalid

--- smallest_to_highest.py
+++ smallest_to_highest.py
@@ -1,14 +1,21 @@
+#try again until the value is valid
+def is_valid(number):
+    while True:
+        try:
+            num = int(input(number))
+            return num
+        except ValueError:
+            print("Invalid input, try again.")
+
 #storage for inputed numbers
 num_storage = []
 #for-loop for asking
 for ask_num in range(1, 11):
-    ask_for_num = int(input(f"{ask_num}.) Enter number: "))
-    num_storage.append(ask_for_num)
-
+    numbers = is_valid(f"{ask_num}.) Enter number: ")
+    num_storage.append(numbers)
 
 #to sort all the values from least to greatest
 srt_lst = sorted(num_storage)
 
 #print statement
 print(srt_lst)
-
